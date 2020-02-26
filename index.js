a178d75a8d5ad74cbc6444eb1efcf46c662e0ffe
Wanderson David <wandersondavid.ifg@gmail.com>
2020-02-26 00:58:26 -0300

    Select prefix

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const readline = require('readline-sync')
 
-function start(){
-    const content = {}
+function start() {
+    const content = {};
 
-    content.searchTerm = askAndReturnSerachTerm()
+    content.searchTerm = askAndReturnSerachTerm();
+    content.prefix = askAndReturnPrefix();
 
-    function askAndReturnSerachTerm(){
-        return readline.question('Type a Wikipedia search term: ')
+    function askAndReturnSerachTerm() {
+        return readline.question('Type a Wikipedia search term: ');
     }
 
-    console.log(content)
+    function askAndReturnPrefix() {
+        const prefixes = ['Who is', 'What is', 'The history of'];
+        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose on option: ') 
+        const selectedPrefixText = prefixes[selectedPrefixIndex]
+        return selectedPrefixText
+    }
+
+    console.log(content);
 }
 start()
