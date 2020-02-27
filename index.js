291c055ac59e467bd0eb9f7f6976832fb6db6f19
Wanderson David <wandersondavid.ifg@gmail.com>
2020-02-27 15:17:49 -0300

    Api de busca no wikipidia

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const readline = require('readline-sync')
-const robots ={
+const robots = {
     text: require('./robots/text.js')
 }
 
@@ -15,7 +15,7 @@ function start() {
 
     function askAndReturnPrefix() {
         const prefixes = ['Who is', 'What is', 'The history of'];
-        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose on option: ') 
+        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose on option: ')
         const selectedPrefixText = prefixes[selectedPrefixIndex]
         return selectedPrefixText
     }
--- robots/text.js
+++ robots/text.js
@@ -1,6 +1,21 @@
+const algorithmia = require('algorithmia')
+// const algorithmiaApiKey = require('../credentio/algorithmia.json')
+// console.log("**************",algorithmiaApiKey.apiKey,"***************************")
 function robot(content) {
-    fetchContentFromWikipedia(content)
-    sanitizeContent(content);
-    breakContentIntoSenteces(content)
+    fetchContentFromWikipidia(content)
+    // sanitizeContent(content);
+    // breakContentIntoSenteces(content);
+
+    async function fetchContentFromWikipidia(content){
+
+        const algorithmiaAuthenticated = algorithmia('simY1VTxIaELpQgOV32Cn1Mm1xp1');
+        const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2');
+        const wikipediaResponde = await wikipediaAlgorithm.pipe(content.searchTerm);
+        const wikipediaContent = wikipediaResponde.get();
+
+        console.log(wikipediaContent);
+    
+    }
+
 }
 module.exports = robot;
