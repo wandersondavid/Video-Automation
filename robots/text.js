6224a8b0a36d70cc94b9bc4678c95f3b5c2dc9f0
Wanderson David <wandersondavid.ifg@gmail.com>
2020-02-28 11:37:45 -0300

    clear  conteudo do wikipidia

--- robots/text.js
+++ robots/text.js
@@ -1,20 +1,39 @@
 const algorithmia = require('algorithmia')
 // const algorithmiaApiKey = require('../credentio/algorithmia.json')
 // console.log("**************",algorithmiaApiKey.apiKey,"***************************")
-function robot(content) {
-    fetchContentFromWikipidia(content)
-    // sanitizeContent(content);
+async function robot(content) {
+    await fetchContentFromWikipidia(content)
+    sanitizeContent(content);
     // breakContentIntoSenteces(content);
 
-    async function fetchContentFromWikipidia(content){
+    async function fetchContentFromWikipidia(content) {
 
         const algorithmiaAuthenticated = algorithmia('simY1VTxIaELpQgOV32Cn1Mm1xp1');
         const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2');
         const wikipediaResponde = await wikipediaAlgorithm.pipe(content.searchTerm);
         const wikipediaContent = wikipediaResponde.get();
 
-        console.log(wikipediaContent);
-    
+        content.sourceContentOriginal = wikipediaContent.content
+
+    }
+    function sanitizeContent(content) {
+        const withoutBlankLinesAndMankLines = removeBlankLinesandMarkdom(content.sourceContentOriginal);
+
+        console.log(withoutBlankLinesAndMankLines);
+
+        function removeBlankLinesandMarkdom(text) {
+            const allLines = text.split('\n');
+
+            const withoutBlankLinesMarkdon = allLines.filter((line) => {
+                if (line.trim().length === 0 || line.trim().startsWith('='))
+                    return false;
+
+                return true;
+            })
+
+            return withoutBlankLinesMarkdon.join(' ');
+
+        }
     }
 
 }
