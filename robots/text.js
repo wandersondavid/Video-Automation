c4193af5f50e242325bbed95a21c2e7d428d1348
Wanderson David <wandersondavid.ifg@gmail.com>
2020-03-03 14:19:49 -0300

    ajuste no codigo

--- robots/text.js
+++ robots/text.js
@@ -1,36 +1,14 @@
 const algorithmia = require('algorithmia')
 const sentenceBoundaryDetection = require('sbd')
-// const watsonApiKey = require('../credentials/watson-nlu').apiKey
+const watsonApiKey = require('../credentials/watson-nlu');
+const algorithmiaApiKey = require('../credentials/algorithimia.json')
 const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js')
-
-
-const watsonApiKey = {
-    "apikey": "ezQVz6JQu3d8MHXfTy9HcXCyxewddNBFqAJpFfYMA1Rr",
-    "iam_apikey_description": "Auto-generated for key 62c3275d-5a1e-4316-b864-6ebf3136cf35",
-    "iam_apikey_name": "Auto-generated service credentials",
-    "iam_role_crn": "crn:v1:bluemix:public:iam::::serviceRole:Manager",
-    "iam_serviceid_crn": "crn:v1:bluemix:public:iam-identity::a/296ccff6b56e44eeb75e92f485031e34::serviceid:ServiceId-995a39a5-0fa9-442f-9b1a-1cf787e5a51b",
-    "url": "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/a380a6e0-6c2b-489c-b78f-bfa06d925967"
-}
 
 const nlu = new NaturalLanguageUnderstandingV1({
     iam_apikey: watsonApiKey.apikey,
     version: '2018-11-16',
     ulr: "https://gateway-lon.watsonplatform.net/natural-language-understanding/api/v1/analyze?"
 })
-
-// nlu.analyze({
-//     text: `Search the world's information, including webpages, images, videos and more. Google has many special features to help you find exactly what you're looking`,
-//     features: {
-//         keywords: {}
-//     }
-// }, (error, response) => {
-//     if (error) {
-//         throw error
-//     }
-//     console.log(JSON.stringify(response, null, 4))
-//     process.exit(0)
-// })
 
 async function robot(content) {
     await fetchContentFromWikipidia(content)
@@ -40,8 +18,8 @@ async function robot(content) {
     await keywordsOfAllSentences(content)
 
     async function fetchContentFromWikipidia(content) {
-
-        const algorithmiaAuthenticated = algorithmia('simY1VTxIaELpQgOV32Cn1Mm1xp1');
+        
+        const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey.apikey);
         const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2');
         const wikipediaResponde = await wikipediaAlgorithm.pipe(content.searchTerm);
         const wikipediaContent = wikipediaResponde.get();
@@ -52,7 +30,6 @@ async function robot(content) {
     function sanitizeContent(content) {
         const withoutBlankLinesAndMankLines = removeBlankLinesandMarkdom(content.sourceContentOriginal);
 
-        // console.log(withoutBlankLinesAndMankLines);
         content.sourceContentSanitized = withoutBlankLinesAndMankLines
         function removeBlankLinesandMarkdom(text) {
             const allLines = text.split('\n');
