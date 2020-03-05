90c2a9b5d638147e3d71ba83095fd86d07c55bc2
Wanderson David <wandersondavid.ifg@gmail.com>
2020-03-05 08:51:35 -0300

    search engine

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const robots = {
     input:require('./robots/input.js'),
     text: require('./robots/text.js'),
-    state: require('./robots/state')
+    state: require('./robots/state'),
+    image: require('./robots/image.js')
 }
+
 async function start() {
     robots.input();
-    await robots.text()
+    await robots.text();
+    await robots.image();
    
     const content = robots.state.load();
     console.dir(content, {depth:null})
