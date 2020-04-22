21b9b2052744e5f9d8d9eb5324ad6877d3c8a962
Wanderson David <wandersondavid.ifg@gmail.com>
2020-04-21 22:20:03 -0300

    feat: robot  in image

--- robots/image.js
+++ robots/image.js
@@ -1,7 +1,32 @@
-const state =require('./state.js');
+const { google } = require('googleapis');
+const customSearch = google.customsearch('v1')
+const state = require('./state.js');
+const googleSearchCredentias = require('../credentials/search-google.json')
 
-async function robot(){
+async function robot() {
     const content = state.load();
+
+    const imagesArry = await fechGoogleAndReturnImagesLinks('Google');
+
+    console.log('======================================================')
+    console.dir(imagesArry, { depth: null })
+    process.exit(0)
+
+    async function fechGoogleAndReturnImagesLinks(query) {
+        const response = await customSearch.cse.list({
+            auth: googleSearchCredentias.apikey,
+            cx: googleSearchCredentias.searchEngineID,
+            q: query,
+            searchType: 'image',
+            num: 2
+        })
+
+        const imageUrl = response.data.items.map((item) => {
+            return item.link
+        })
+        return imageUrl
+    }
+
 }
 
 module.exports = robot
