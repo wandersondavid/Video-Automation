2d09cda06bc7d143140155fd3d1b34a6f2f64259
Wanderson David <wandersondavid.ifg@gmail.com>
2020-04-26 00:46:49 -0300

    feat: add sentences image

--- robots/image.js
+++ robots/image.js
@@ -9,7 +9,9 @@ async function robot() {
     const content = state.load();
     // await fetchImageOfAllSentences(content);
     // await downloadAllImages(content);
-    await convertAllImages(content)
+    // await convertAllImages(content)
+
+    await createAllSenteceImages(content)
 
     // state.save(content);
 
@@ -109,6 +111,66 @@ async function robot() {
             })
         })
     }
+    async function createAllSenteceImages(content) {
+        for(let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++){
+            await createSentenceImage(sentenceIndex, content.sentences[sentenceIndex].text)
+        }
+    }
+
+    async function createSentenceImage(sentenceIndex, sentenceText) {
+        
+        return new Promise((resolve, reject) => {
+            const outputFile = `./content/${sentenceIndex}-sentence.png`
+
+            const templateSettings = {
+                0: {
+                size: '1920x400',
+                gravity: 'center'
+                },
+                1: {
+                size: '1920x1080',
+                gravity: 'center'
+                },
+                2: {
+                size: '800x1080',
+                gravity: 'west'
+                },
+                3: {
+                size: '1920x400',
+                gravity: 'center'
+                },
+                4: {
+                size: '1920x1080',
+                gravity: 'center'
+                },
+                5: {
+                size: '800x1080',
+                gravity: 'west'
+                },
+                6: {
+                size: '1920x400',
+                gravity: 'center'
+                }
+
+            }
+
+            gm()
+                .out('-size', templateSettings[sentenceIndex].size)
+                .out('-gravity', templateSettings[sentenceIndex].gravity)
+                .out('-background', 'transparent')
+                .out('-fill', 'white')
+                .out('-kerning', '-1')
+                .out(`caption:${sentenceText}`)
+                .write(outputFile, (error) => {
+                if (error) {
+                    return reject(error)
+                }
+
+                console.log(`> [video-robot] Sentence created: ${outputFile}`)
+                resolve()
+                })
+            })
+        }
 }
 
 module.exports = robot
